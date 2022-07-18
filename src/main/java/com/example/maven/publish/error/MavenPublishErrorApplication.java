41db2819fa60df8aee928963daaabf261b171d00
Kirill Verevkin <verevkin.ka@esphere.ru>
2022-07-18 14:26:05 +0300

    Init commit

---
+++ src/main/java/com/example/maven/publish/error/MavenPublishErrorApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven.publish.error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPublishErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPublishErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maven/publish/error/MavenPublishErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven.publish.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MavenPublishErrorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
